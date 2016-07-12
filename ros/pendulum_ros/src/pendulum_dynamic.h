3d4454c62f927bf8cfb588ffec4ceeb4127f4b12
mingliang.wu <mingliang.wu@ck-telecom.com>
2016-07-12 19:53:48 +0800

    [BugFix] correct formula 7

--- ros/pendulum_ros/src/pendulum_dynamic.h
+++ ros/pendulum_ros/src/pendulum_dynamic.h
@@ -5,29 +5,24 @@
 #include <geometry_msgs/Vector3.h>
 #include <math.h>
 
-/*
-formula 12:
 
-syms r r_vel r_acc s s_vel s_acc x_acc y_acc L g;
-l=sqrt(L^2-r^2-s^2)
-z_acc=0
-g=9.8
-
-f1 =
- 
-r_acc == -(x_acc*(L^2 - s^2)^2 - 2*r^2*(x_acc*(L^2 - s^2) - r_vel*s*s_vel) + r^4*x_acc + r*(L^2*(r_vel^2 + s_vel^2 - l*(g + z_acc)) - s^3*s_acc + s^2*(- r_vel^2 + l*(g + z_acc)) + L^2*s*s_acc) - r^3*(s_vel^2 - l*(g + z_acc) + s*s_acc))/(l^2*(L^2 - s^2))
-	  
-f2 =
-	   
-s_acc == -(y_acc*(L^2 - r^2)^2 - 2*s^2*(y_acc*(L^2 - r^2) - r*r_vel*s_vel) + s^4*y_acc + s*(L^2*(r_vel^2 + s_vel^2 - l*(g + z_acc)) - r^3*r_acc + r^2*(- s_vel^2 + l*(g + z_acc)) + L^2*r*r_acc) - s^3*(r_vel^2 - l*(g + z_acc) + r*r_acc))/(l^2*(L^2 - s^2))
-	    
-
-*/
 
 
 /*
-formula 5:
 
+    |1 0            0            |
+RMx=|0 cos(angle_x) -sin(angle_x)|
+    |0 sin(angle_x) cos(angle_x) |
+
+    |cos(angle_y)  0 sin(angle_y)|
+RMy=|0             1 0           |
+    |-sin(angle_y) 0 cos(angle_y)|
+
+    |cos(angle_z) -sin(angle_z) 0|
+RMz=|sin(angle_z) cos(angle_z)  0|
+    |0            0             1|
+
+double g = 9.81;
 
 */
 
@@ -35,8 +30,22 @@ class PendulumDynamic {
 
 public:
 
-	static const double g = 9.8;
+	static const double g = 9.81;
 
+	/*
+	formula 12:
+
+	syms r r_vel r_acc s s_vel s_acc x_acc y_acc L g;
+	l=sqrt(L^2-r^2-s^2)
+	z_acc=0
+
+	f1 =
+	r_acc == -(x_acc*(L^2 - s^2)^2 - 2*r^2*(x_acc*(L^2 - s^2) - r_vel*s*s_vel) + r^4*x_acc + r*(L^2*(r_vel^2 + s_vel^2 - l*(g + z_acc)) - s^3*s_acc + s^2*(- r_vel^2 + l*(g + z_acc)) + L^2*s*s_acc) - r^3*(s_vel^2 - l*(g + z_acc) + s*s_acc))/(l^2*(L^2 - s^2))
+
+	f2 =
+	s_acc == -(y_acc*(L^2 - r^2)^2 - 2*s^2*(y_acc*(L^2 - r^2) - r*r_vel*s_vel) + s^4*y_acc + s*(L^2*(r_vel^2 + s_vel^2 - l*(g + z_acc)) - r^3*r_acc + r^2*(- s_vel^2 + l*(g + z_acc)) + L^2*r*r_acc) - s^3*(r_vel^2 - l*(g + z_acc) + r*r_acc))/(l^2*(L^2 - s^2))
+
+	*/
 	static void formula_12(const double pendulum_l,
 						const double pendulum_x,
 						const double pendulum_y,
@@ -44,6 +53,7 @@ public:
 						const geometry_msgs::Vector3& pendulum_vel_acc,
 						double* vehicle_vel_acc_x,
 						double* vehicle_vel_acc_y) {
+
 		double l = sqrt(pow(pendulum_l, 2) - pow(pendulum_x, 2) - pow(pendulum_y, 2));
 		double z_acc = 0;
 
@@ -95,84 +105,117 @@ public:
 			2*s_2*(L_2 - r_2) + s_4);
 	}
 
+	/*
+	version 1:
+
+	|vel_acc_x|             |0|   |0|
+	|vel_acc_y|=RMx*RMy*RMz*|0| + |0|
+	|vel_acc_z|             |a|   |g|
+=>
+	|vel_acc_x|  |a*sin(angle_y)                 |
+	|vel_acc_y|= |-a*cos(angle_y)*sin(angle_x)   |
+	|vel_acc_z|  |a*cos(angle_x)*cos(angle_y) - g|
+=>
+	vel_acc_x=a*sin(angle_y)
+	vel_acc_y=-a*cos(angle_y)*sin(angle_x)
+	vel_acc_z=a*cos(angle_y)*cos(angle_x)-g
+	vel_acc_z=0
+
+	*/
 	static void formula_5(const double vel_acc_x, const double vel_acc_y,
 						double* angle_x, double* angle_y, double* a) {
-		/*
-
-		version 1:
-		vel_acc_x=a*sin(angle_y)
-		vel_acc_y=-a*cos(angle_y)*sin(angle_x)
-		vel_acc_z=a*cos(angle_y)*cos(angle_x)-g
-		vel_acc_z=0
 
 		*angle_x = atan(-vel_acc_y/g);
 		*angle_y = atan(-vel_acc_x*sin(*angle_x)/vel_acc_y);
 		*a = vel_acc_x/sin(*angle_y);
+	}
 
-		*/
+	/*
+	version 2:
 
-		/*
-		version 2:
-		    |1 0            0            |
-		RMx=|0 cos(angle_x) -sin(angle_x)|
-		    |0 sin(angle_x) cos(angle_x) |
+	|vel_acc_x|             |0|   |0|
+	|vel_acc_y|=RMz*RMy*RMx*|0| + |0|
+	|vel_acc_z|             |a|   |g|
+=>
+	|vel_acc_x| |cos(angle_z) -sin(angle_z) 0|   |cos(angle_y)  0 sin(angle_y)|   |1 0            0            |   |0|   |0|
+	|vel_acc_y|=|sin(angle_z) cos(angle_z)  0| * |0             1 0           | * |0 cos(angle_x) -sin(angle_x)| * |0| + |0|
+	|vel_acc_z| |0            0             1|   |-sin(angle_y) 0 cos(angle_y)|   |0 sin(angle_x) cos(angle_x) |   |a|   |g|
 
-		    |cos(angle_y)  0 sin(angle_y)|
-		RMy=|0             1 0           |
-		    |-sin(angle_y) 0 cos(angle_y)|
+	            |a*(sin(angle_x)*sin(angle_z) + cos(angle_x)*cos(angle_z)*sin(angle_y)) |
+	           =|-a*(cos(angle_z)*sin(angle_x) - cos(angle_x)*sin(angle_y)*sin(angle_z))|
+	            |a*cos(angle_x)*cos(angle_y) - g                                        |
 
-		    |cos(angle_z) -sin(angle_z) 0|
-		RMz=|sin(angle_z) cos(angle_z)  0|
-		    |0            0             1|
+	vel_acc_z=0
+	angle_z=0
+	cos(angle_z)=1
+	sin(angle_z)=0
+=>
+	vel_acc_x=a*cos(angle_x)*sin(angle_y)
+	vel_acc_y=-a*sin(angle_x)
+	0=a*cos(angle_x)cos(angle_y)-g
 
-		|vel_acc_x|             |0|   |0|
-		|vel_acc_y|=RMz*RMy*RMx*|0| + |0|
-		|vel_acc_z|             |a|   |g|
+	*/
+	static void formula_5_2(const double vel_acc_x, const double vel_acc_y,
+						double* angle_x, double* angle_y, double* a) {
 
-
-		|vel_acc_x| |cos(angle_z) -sin(angle_z) 0|   |cos(angle_y)  0 sin(angle_y)|   |1 0            0            |   |0|   |0|
-		|vel_acc_y|=|sin(angle_z) cos(angle_z)  0| * |0             1 0           | * |0 cos(angle_x) -sin(angle_x)| * |0| + |0|
-		|vel_acc_z| |0            0             1|   |-sin(angle_y) 0 cos(angle_y)|   |0 sin(angle_x) cos(angle_x) |   |a|   |g|
-
-		            |a*(sin(angle_x)*sin(angle_z) + cos(angle_x)*cos(angle_z)*sin(angle_y)) |
-		           =|-a*(cos(angle_z)*sin(angle_x) - cos(angle_x)*sin(angle_y)*sin(angle_z))|
-		            |a*cos(angle_x)*cos(angle_y) - g                                        |
-
-		vel_acc_z=0
-		angle_z=0
-		cos(angle_z)=1
-		sin(angle_z)=0
-
-		==>
-
-		vel_acc_x=a*cos(angle_x)*sin(angle_y)
-		vel_acc_y=-a*sin(angle_x)
-		0=a*cos(angle_x)cos(angle_y)-g
-
-		=>
-		*/
 
 		*angle_y = atan(vel_acc_x/g);
 		*angle_x = atan(-vel_acc_y*sin(*angle_y)/vel_acc_x);
 		*a = -vel_acc_y/sin(*angle_x);
 	}
 
+	/*
+	version 1:
+
+	| rate_x |   | cos(angle_y)*cos(angle_x)	-sin(angle_x)	0 |-1    | vehicle_rate_x |
+	| rate_y | = | cos(angle_y)*sin(angle_x)	cos(angle_x)	0 |   *  | vehicle_rate_y |
+	| rate_z |   | -sin(angle_y)				0				1 |      | vehicle_rate_z |
+=>
+	| rate_x*cos(angle_x)*cos(angle_y) - rate_y*sin(angle_x) |   | vehicle_rate_x |
+	| rate_y*cos(angle_x) + rate_x*cos(angle_y)*sin(angle_x) | = | vehicle_rate_y |
+	| rate_z - rate_x*sin(angle_y)                           |   | vehicle_rate_z |
+
+	*/
 	static void formula_7(const double angle_x, const double angle_y,
 						const double rate_x, const double rate_y,
 						double* vehicle_rate_x, double* vehicle_rate_y) {
-		/*
 
-		| rate_x |   | cos(angle_y)*cos(angle_x)	-sin(angle_x)	0 |-1    | vehicle_rate_x |
-		| rate_y | = | cos(angle_y)*sin(angle_x)	cos(angle_x)	0 |   *  | vehicle_rate_y |
-		| rate_z |   | -sin(angle_y)				0				1 |      | vehicle_rate_z |
 
-		| rate_x*cos(angle_x)*cos(angle_y) - rate_y*sin(angle_x) |   | vehicle_rate_x |
-		| rate_y*cos(angle_x) + rate_x*cos(angle_y)*sin(angle_x) | = | vehicle_rate_y |
-		| rate_z - rate_x*sin(angle_y)                           |   | vehicle_rate_z |
-
-		*/
 		*vehicle_rate_x = rate_x*cos(angle_x)*cos(angle_y) - rate_y*sin(angle_x);
 		*vehicle_rate_y = rate_y*cos(angle_x) + rate_x*cos(angle_y)*sin(angle_x);
+	}
+
+	/*
+	version 2:
+
+	|vehicle_rate_x|   |rate_x|            |0     |                     |0     |
+	|vehicle_rate_y| = |0     | + RMx^-1 * |rate_y| + RMx^-1 * RMy^-1 * |0     |
+	|vehicle_rate_z|   |0     |            |0     |                     |rate_z|
+=>
+	|vehicle_rate_x|   |rate_x|   |1 0             0           |   |0     |
+	|vehicle_rate_y| = |0     | + |0 cos(angle_x)  sin(angle_x)| * |rate_y|
+	|vehicle_rate_z|   |0     |   |0 -sin(angle_x) cos(angle_x)|   |0     |
+
+	                              |1 0             0           |   |cos(angle_y) 0 -sin(angle_y)|   |0     |
+	                            + |0 cos(angle_x)  sin(angle_x)| * |0            1 0            | * |0     |
+	                              |0 -sin(angle_x) cos(angle_x)|   |sin(angle_y) 0 cos(angle_y) |   |rate_z|
+=>
+	|vehicle_rate_x|   |rate_x + 0 - sin(angle_y)*rate_z                          |
+	|vehicle_rate_y| = |0 + cos(angle_x)*rate_y + sin(angle_x)*cos(angle_y)*rate_z|
+	|vehicle_rate_z|   |0 - sin(angle_x)*rate_y + cos(angle_x)*cos(angle_y)*rate_z|
+=>
+	|vehicle_rate_x|   |1 0             -sin(angle_y)            |   |rate_x|
+	|vehicle_rate_y| = |0 cos(angle_x)  sin(angle_x)*cos(angle_y)| * |rate_y|
+	|vehicle_rate_z|   |0 -sin(angle_x) cos(angle_x)*cos(angle_y)|   |rate_z|
+
+	*/
+	static void formula_7_2(const double angle_x, const double angle_y,
+						const double rate_x, const double rate_y,
+						double* vehicle_rate_x, double* vehicle_rate_y) {
+
+		double rate_z = 0;
+		*vehicle_rate_x = rate_x - sin(angle_y)*rate_z;
+		*vehicle_rate_y = cos(angle_x)*rate_y + sin(angle_x)*cos(angle_y)*rate_z;
 	}
 };
 
