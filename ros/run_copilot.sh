91ffb2f73778a3264aa17ce5068963e8dd0d75a1
mingliang.wu <mingliang.wu@ck-telecom.com>
2016-07-14 15:36:39 +0800

    [BugFix] adjust shell scripts

--- ros/run_copilot.sh
+++ ros/run_copilot.sh
@@ -1,9 +1,15 @@
 #! /bin/bash                                                                                                                                       
 
-source /opt/ros/indigo/setup.bash
-source devel/setup.bash
+#run in dir catkin_ws
 
-gnome-terminal \
---tab --title "pose_adapter" --command "bash -c 'rosrun pose_adapter pose_adapter_node;'" \
---tab --title "pendulum_ros" --command "bash -c 'rosrun pendulum_ros pendulum_ros_ctrl;'" \
---tab --title "pendulum_cfg" --command "bash -c 'rosrun rqt_reconfigure rqt_reconfigure;'"
+STACK_PATH=./
+source /opt/ros/indigo/setup.bash
+source ${STACK_PATH}/devel/setup.bash
+
+gnome-terminal  \
+	--tab --title "pendulum_ctrl"	--command "bash -c \"
+rosrun pendulum_ros pendulum_ros_ctrl;
+						exec bash\"" \
+	--tab --title "pendulum_cfg"	--command "bash -c \"
+rosrun rqt_reconfigure rqt_reconfigure;
+						exec bash\"" &
